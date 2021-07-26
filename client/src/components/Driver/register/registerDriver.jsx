8637f031f47fdcef285f6a37cd7d999e5c5af70e
Mohammed Almallahi <almallahi@outlook.com>
2021-07-27 00:35:31 +0300

    aa

--- client/src/components/Driver/register/registerDriver.jsx
+++ client/src/components/Driver/register/registerDriver.jsx
@@ -88,7 +88,7 @@ const RegisterDriver = (props) => {
             <input
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
-              value="Save"
+              value="Add driver"
             />
           </div>
         </form>
